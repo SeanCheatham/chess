906def6e04afb7eba5c912ed6f41ea8b501db092
SeanCheatham <smcheath@syr.edu>
2013-12-19 20:32:59 -0500

    Started working on capture calculations

--- src/chess/Chess.java
+++ src/chess/Chess.java
@@ -4,45 +4,54 @@ public class Chess {
 	public static void main(String[] args) {
 		Board b = new Board(8,8);
 		//White's back line
-		b.squares[0][0] = new Location(0,0,new Piece(new Type("R"),0));
-		b.squares[1][0] = new Location(1,0,new Piece(new Type("N"),0));
-		b.squares[2][0] = new Location(2,0,new Piece(new Type("B"),0));
-		b.squares[3][0] = new Location(3,0,new Piece(new Type("Q"),0));
-		b.squares[4][0] = new Location(4,0,new Piece(new Type("K"),0));
-		b.squares[5][0] = new Location(5,0,new Piece(new Type("B"),0));
-		b.squares[6][0] = new Location(6,0,new Piece(new Type("N"),0));
-		b.squares[7][0] = new Location(7,0,new Piece(new Type("R"),0));
+		b.squares[0][0].setOccupant(new Piece(new Type("R"),0,b));
+		b.squares[1][0].setOccupant(new Piece(new Type("N"),0,b));
+		b.squares[2][0].setOccupant(new Piece(new Type("B"),0,b));
+		b.squares[3][0].setOccupant(new Piece(new Type("Q"),0,b));
+		b.squares[4][0].setOccupant(new Piece(new Type("K"),0,b));
+		b.squares[5][0].setOccupant(new Piece(new Type("B"),0,b));
+		b.squares[6][0].setOccupant(new Piece(new Type("N"),0,b));
+		b.squares[7][0].setOccupant(new Piece(new Type("R"),0,b));
 		//White's front line
-		b.squares[0][1] = new Location(0,1,new Piece(new Type("P"),0));
-		b.squares[1][1] = new Location(1,1,new Piece(new Type("P"),0));
-		b.squares[2][1] = new Location(2,1,new Piece(new Type("P"),0));
-		b.squares[3][1] = new Location(3,1,new Piece(new Type("P"),0));
-		b.squares[4][1] = new Location(4,1,new Piece(new Type("P"),0));
-		b.squares[5][1] = new Location(5,1,new Piece(new Type("P"),0));
-		b.squares[6][1] = new Location(6,1,new Piece(new Type("P"),0));
-		b.squares[7][1] = new Location(7,1,new Piece(new Type("P"),0));
+		b.squares[0][1].setOccupant(new Piece(new Type("P"),0,b));
+		b.squares[1][1].setOccupant(new Piece(new Type("P"),0,b));
+		b.squares[2][1].setOccupant(new Piece(new Type("P"),0,b));
+		b.squares[3][1].setOccupant(new Piece(new Type("P"),0,b));
+		b.squares[4][1].setOccupant(new Piece(new Type("P"),0,b));
+		b.squares[5][1].setOccupant(new Piece(new Type("P"),0,b));
+		b.squares[6][1].setOccupant(new Piece(new Type("P"),0,b));
+		b.squares[7][1].setOccupant(new Piece(new Type("P"),0,b));
 		//Black's front line
-		b.squares[0][6] = new Location(0,6,new Piece(new Type("P"),1));
-		b.squares[1][6] = new Location(1,6,new Piece(new Type("P"),1));
-		b.squares[2][6] = new Location(2,6,new Piece(new Type("P"),1));
-		b.squares[3][6] = new Location(3,6,new Piece(new Type("P"),1));
-		b.squares[4][6] = new Location(4,6,new Piece(new Type("P"),1));
-		b.squares[5][6] = new Location(5,6,new Piece(new Type("P"),1));
-		b.squares[6][6] = new Location(6,6,new Piece(new Type("P"),1));
-		b.squares[7][6] = new Location(7,6,new Piece(new Type("P"),1));
+		b.squares[0][6].setOccupant(new Piece(new Type("P"),1,b));
+		b.squares[1][6].setOccupant(new Piece(new Type("P"),1,b));
+		b.squares[2][6].setOccupant(new Piece(new Type("P"),1,b));
+		b.squares[3][6].setOccupant(new Piece(new Type("P"),1,b));
+		b.squares[4][6].setOccupant(new Piece(new Type("P"),1,b));
+		b.squares[5][6].setOccupant(new Piece(new Type("P"),1,b));
+		b.squares[6][6].setOccupant(new Piece(new Type("P"),1,b));
+		b.squares[7][6].setOccupant(new Piece(new Type("P"),1,b));
 		//Black's back line
-		b.squares[0][7] = new Location(0,7,new Piece(new Type("R"),1));
-		b.squares[1][7] = new Location(1,7,new Piece(new Type("N"),1));
-		b.squares[2][7] = new Location(2,7,new Piece(new Type("B"),1));
-		b.squares[3][7] = new Location(3,7,new Piece(new Type("Q"),1));
-		b.squares[4][7] = new Location(4,7,new Piece(new Type("K"),1));
-		b.squares[5][7] = new Location(5,7,new Piece(new Type("B"),1));
-		b.squares[6][7] = new Location(6,7,new Piece(new Type("N"),1));
-		b.squares[7][7] = new Location(7,7,new Piece(new Type("R"),1));
+		b.squares[0][7].setOccupant(new Piece(new Type("R"),1,b));
+		b.squares[1][7].setOccupant(new Piece(new Type("N"),1,b));
+		b.squares[2][7].setOccupant(new Piece(new Type("B"),1,b));
+		b.squares[3][7].setOccupant(new Piece(new Type("Q"),1,b));
+		b.squares[4][7].setOccupant(new Piece(new Type("K"),1,b));
+		b.squares[5][7].setOccupant(new Piece(new Type("B"),1,b));
+		b.squares[6][7].setOccupant(new Piece(new Type("N"),1,b));
+		b.squares[7][7].setOccupant(new Piece(new Type("R"),1,b));
 		b.drawBoard();
 		//b.listPieces();
 		
-		b.movePiece(b.squares[0][1], 0, 2);
-		b.drawBoard();
+		/*b.movePiece(b.squares[0][1], 0, 2);
+		b.drawBoard();*/
+		
+		//Location to check
+		Location p1 = b.squares[1][1];
+		System.out.println("Calculating possible moves for: "+p1);
+		p1.occupant.calculateAttackableSquares(b.squares[1][1]);
+		for(int i=0; i<=p1.occupant.calculateAttackableSquares(p1).size()-1; i++){
+			System.out.println(p1.occupant.calculateAttackableSquares(p1).get(i).chessCoordinates());
+		}
+		
 	}
 }
--- src/chess/Piece.java
+++ src/chess/Piece.java
@@ -1,11 +1,28 @@
 package chess;
 
+import java.util.*;
+
 public class Piece {
 	public Type type;
 	public int team;
+	public Board board;
+	public ArrayList<Location> attackbleSquares;
 	
-	public Piece(Type ty, int t) {
+	public Piece(Type ty, int t, Board b) {
 		type = ty;
 		team = t;
+		board = b;
+	}
+	
+	public ArrayList<Location> calculateAttackableSquares(Location loc){
+		ArrayList<Location> vals = new ArrayList<Location>();
+		Location currentLocation = loc;
+		if(this.type.shortName == "P"){
+			if(this.team == 0){
+				if(currentLocation.file+1 < this.board.width-1 && currentLocation.rank+1 < this.board.height && this.board.squares[currentLocation.file+1][currentLocation.rank+1].occupant == null) vals.add(this.board.squares[currentLocation.file+1][currentLocation.rank+1]);
+				if(currentLocation.file-1 >= 0 && currentLocation.rank+1 < this.board.height && this.board.squares[currentLocation.file-1][currentLocation.rank+1].occupant == null) vals.add(this.board.squares[currentLocation.file-1][currentLocation.rank+1]);
+			}
+		}
+		return vals;
 	}
 }
